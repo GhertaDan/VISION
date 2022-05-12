7ca4ae907c261953db15257379d7a9ad093156c0
GhertaDan <96659244+GhertaDan@users.noreply.github.com>
2022-05-12 15:14:07 +0300

    cart section added

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,11 @@
 // Подключаем слайдер Swiper из node_modules
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {
+	Navigation,
+	Pagination,
+	Autoplay
+} from 'swiper';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay, 
@@ -32,7 +36,7 @@ function initSliders() {
 		new Swiper('.swiper', { // Указываем скласс нужного слайдера
 			// Подключаем модули слайдера
 			// для конкретного случая
-			modules: [Navigation, Pagination],
+			modules: [Navigation, Pagination, Autoplay],
 			observer: true,
 			observeParents: true,
 			slidesPerView: 1,
@@ -46,14 +50,14 @@ function initSliders() {
 			//preloadImages: false,
 			//lazy: true,
 
-			/*
+
 			// Эффекты
-			effect: 'fade',
+			// effect: 'fade',
 			autoplay: {
-				delay: 3000,
+				delay: 2000,
 				disableOnInteraction: false,
 			},
-			*/
+
 
 			// Пагинация
 
