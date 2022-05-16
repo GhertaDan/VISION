c6a317492a0583e3a81e221d54d5f5beaf08e3f8
GhertaDan <deepdanny2003@gmail.com>
2022-05-16 07:16:06 +0300

    register page

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,3 +3,15 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+const signUp = e => {
+    let formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        pass: document.getElementById('pass').value,
+        confirmPass: document.getElementById('confirmPass').value,
+    }
+    localStorage.setItem('formData', JSON.stringify(formData));
+    console.log(localStorage.getItem('formData'));
+    e.preventDefault();
+}
