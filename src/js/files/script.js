5ebfac32afcbbd4de50e7de3f2f5de2545bdf3ab
GhertaDan <96659244+GhertaDan@users.noreply.github.com>
2022-05-18 14:09:30 +0300

    product page + adaptive

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -7,6 +7,13 @@ import {
     flsModules
 } from "./modules.js";
 
+let product = document.querySelectorAll(".card__img");
+product.forEach(el => {
+    el.addEventListener("click", e => {
+        window.location.href = "product.html";
+    })
+})
+
 let favorite = document.querySelectorAll('.card__icon');
 console.log(favorite);
 
